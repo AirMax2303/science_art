16ef919747d61988463f519f3f93635023a8b1fd
LysenkoWork <lyss.work@gmail.com>
2023-04-03 09:37:10 +0300

    Модель данных

---
+++ lib/data/candidate_model.dart
@@ -0,0 +1,21 @@
+class Candidate {
+  Candidate({
+    required this.id,
+    required this.fio,
+    required this.age_category,
+    required this.job,
+    required this.email,
+    required this.section,
+    required this.phone_number,
+    required this.leadership,
+  });
+
+  final int id;
+  final String fio;
+  final String age_category;
+  final String job;
+  final String email;
+  final String section;
+  final String phone_number;
+  final String leadership;
+}
