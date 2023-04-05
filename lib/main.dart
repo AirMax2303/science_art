5ec988abc8af343d4e5da286af7feba0b0326a10
LysenkoWork <lyss.work@gmail.com>
2023-04-05 18:34:38 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -62,18 +62,18 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class SqliteService {
-  Future<Database> initializeDB() async {
-    String path = await getDatabasesPath();
-
-    return openDatabase(
-      join(path, 'database.db'),
-      onCreate: (database, version) async {
-        await database.execute(
-          "CREATE TABLE Notes(id INTEGER PRIMARY KEY AUTOINCREMENT,description TEXT NOT NULL)",
-        );
-      },
-      version: 1,
-    );
-  }
-}
+//class SqliteService {
+//  Future<Database> initializeDB() async {
+//    String path = await getDatabasesPath();
+//
+//    return openDatabase(
+//      join(path, 'database.db'),
+//      onCreate: (database, version) async {
+//        await database.execute(
+//          "CREATE TABLE Notes(id INTEGER PRIMARY KEY AUTOINCREMENT,description TEXT NOT NULL)",
+//        );
+//      },
+//      version: 1,
+//    );
+//  }
+//}
