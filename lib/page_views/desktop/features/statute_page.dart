0ec924671eb31361fbdff66df047c0ff3f347bd4
LysenkoWork <lyss.work@gmail.com>
2023-04-07 18:56:39 +0300

    update

--- lib/page_views/desktop/features/statute_page.dart
+++ lib/page_views/desktop/features/statute_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../../app/theme/app_pallete.dart';
-import '../desktop_view.dart';
 import '../widgets/header_widget.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class StatutePage extends StatefulWidget {
   const StatutePage({Key? key}) : super(key: key);
@@ -42,91 +42,92 @@ class _StatutePageState extends State<StatutePage> {
             //    ],
             //  ),
             //),
-            Padding(
-              padding: EdgeInsets.only(
-                left: mediaQuery.size.width / 90,
-                right: mediaQuery.size.width / 90,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        'Организаторы',
-                        style: simpleText.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(width: 10),
-                      Text(
-                        'ФГБОУ  ВО «Кубанский государственный университет»:',
-                        style: simpleText,
-                      ),
-                    ],
-                  ),
-                  Text(
-                    'Кафедра ДПИ и дизайна художественно-графического факультета;',
-                    style: simpleText,
-                  ),
-                  Text(
-                    'Кафедра анализа данных и искусственного интеллекта факультета компьютерных технологий и прикладной математики;',
-                    style: simpleText,
-                  ),
-                  Text(
-                    'Кафедра истории и правового регулирования массовых коммуникаций факультета журналистики;',
-                    style: simpleText,
-                  ),
-                  Text(
-                    'МУ ДО «Детская художественная школа» им.В.А.Филиппова муниципального образования город Краснодар',
-                    style: simpleText,
-                  ),
-                  SizedBox(height: mediaQuery.size.width / 25),
-                  Wrap(
-                    children: [
-                      Text(
-                        'Партнёры: ',
-                        style: simpleText.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        'ООО Экстраверт, ',
-                        style: simpleText,
-                      ),
-                      Text(
-                        'Краснодарское региональное отделение общественной организации «Союз дизайнеров ',
-                        style: simpleText,
-                      ),
-                      Text(
-                        'России», ',
-                        style: simpleText,
-                      ),
-                      Text(
-                        'Технопарк КубГУ, ',
-                        style: simpleText,
-                      ),
-                      Text(
-                        'Точка кипения  КубГУ, ',
-                        style: simpleText,
-                      ),
-                      Text(
-                        'ООО «Рекламный дом Кубани», ',
-                        style: simpleText,
-                      ),
-                      Text(
-                        'информационный портал cityposter.ru, ',
-                        style: simpleText,
-                      ),
-                      Text(
-                        'газета “Кубанские новости», ',
-                        style: simpleText,
-                      ),
-                      Text(
-                        'телеграмм-канал «Наука и точка»',
-                        style: simpleText,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            HtmlWidget(''),
+            //Padding(
+            //  padding: EdgeInsets.only(
+            //    left: mediaQuery.size.width / 90,
+            //    right: mediaQuery.size.width / 90,
+            //  ),
+            //  child: Column(
+            //    crossAxisAlignment: CrossAxisAlignment.start,
+            //    children: [
+            //      Row(
+            //        children: [
+            //          Text(
+            //            'Организаторы',
+            //            style: simpleText.copyWith(fontWeight: FontWeight.bold),
+            //          ),
+            //          const SizedBox(width: 10),
+            //          Text(
+            //            'ФГБОУ  ВО «Кубанский государственный университет»:',
+            //            style: simpleText,
+            //          ),
+            //        ],
+            //      ),
+            //      Text(
+            //        'Кафедра ДПИ и дизайна художественно-графического факультета;',
+            //        style: simpleText,
+            //      ),
+            //      Text(
+            //        'Кафедра анализа данных и искусственного интеллекта факультета компьютерных технологий и прикладной математики;',
+            //        style: simpleText,
+            //      ),
+            //      Text(
+            //        'Кафедра истории и правового регулирования массовых коммуникаций факультета журналистики;',
+            //        style: simpleText,
+            //      ),
+            //      Text(
+            //        'МУ ДО «Детская художественная школа» им.В.А.Филиппова муниципального образования город Краснодар',
+            //        style: simpleText,
+            //      ),
+            //      SizedBox(height: mediaQuery.size.width / 25),
+            //      Wrap(
+            //        children: [
+            //          Text(
+            //            'Партнёры: ',
+            //            style: simpleText.copyWith(fontWeight: FontWeight.bold),
+            //          ),
+            //          Text(
+            //            'ООО Экстраверт, ',
+            //            style: simpleText,
+            //          ),
+            //          Text(
+            //            'Краснодарское региональное отделение общественной организации «Союз дизайнеров ',
+            //            style: simpleText,
+            //          ),
+            //          Text(
+            //            'России», ',
+            //            style: simpleText,
+            //          ),
+            //          Text(
+            //            'Технопарк КубГУ, ',
+            //            style: simpleText,
+            //          ),
+            //          Text(
+            //            'Точка кипения  КубГУ, ',
+            //            style: simpleText,
+            //          ),
+            //          Text(
+            //            'ООО «Рекламный дом Кубани», ',
+            //            style: simpleText,
+            //          ),
+            //          Text(
+            //            'информационный портал cityposter.ru, ',
+            //            style: simpleText,
+            //          ),
+            //          Text(
+            //            'газета “Кубанские новости», ',
+            //            style: simpleText,
+            //          ),
+            //          Text(
+            //            'телеграмм-канал «Наука и точка»',
+            //            style: simpleText,
+            //          ),
+            //        ],
+            //      ),
+            //    ],
+            //  ),
+            //),
           ],
         ),
       ),
